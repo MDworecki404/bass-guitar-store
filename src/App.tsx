5f69c50046e998e0b6e1f580cedfb5a9f7453f80
MDWebDevelopment <md.web.development404@gmail.com>
2023-01-11 14:56:14 +0100

    Background video

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,71 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import "./App.css";
+import styled from "styled-components";
+import React from "react";
+import background from "./assets/background.mp4";
+import gsap from "gsap";
 
-function App() {
-  const [count, setCount] = useState(0)
+const Header = styled.header`
+  width: 100vw;
+  height: 100vh;
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+  #videoBG {
+    width: 100vw;
+    object-fit: cover;
+    position: fixed;
+    z-index: -100;
+  }
+
+  p {
+    width: 100%;
+    height: 80%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: white;
+    font-size: 2.5rem;
+  }
+  .exploreButton {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 20%;
+    color: white;
+    font-size: 2rem;
+    span {
+      width: 10vw;
+      height: 8vh;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      color: white;
+      border: 2px solid white;
+      transition: all 0.3s ease-in-out;
+      &:hover {
+        background-color: white;
+        color: black;
+        cursor: pointer;
+      }
+    }
+  }
+`;
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Header>
+          <video autoPlay muted loop id="videoBG">
+            <source src={background} />
+          </video>
+          <p>Bass Guitar Store</p>
+          <p className="exploreButton">
+            <span>Explore</span>
+          </p>
+        </Header>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    );
+  }
 }
 
-export default App
+export default App;
