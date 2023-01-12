763758f91a9a859c8a976a3f7488bc4192f8ddb6
MDWebDevelopment <md.web.development404@gmail.com>
2023-01-12 11:53:50 +0100

    Animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import styled from "styled-components";
-import React from "react";
+import React, { useRef } from "react";
 import background from "./assets/background.mp4";
 import gsap from "gsap";
 
@@ -9,6 +9,7 @@ const Header = styled.header`
   height: 100vh;
 
   #videoBG {
+    height: 100vh;
     width: 100vw;
     object-fit: cover;
     position: fixed;
@@ -32,40 +33,81 @@ const Header = styled.header`
     height: 20%;
     color: white;
     font-size: 2rem;
-    span {
+    button {
       width: 10vw;
       height: 8vh;
       display: flex;
       justify-content: center;
       align-items: center;
+      background: none;
+      border: none;
+      z-index: 1;
+      font-size: 2rem;
       color: white;
-      border: 2px solid white;
       transition: all 0.3s ease-in-out;
+      cursor: pointer;
+
       &:hover {
-        background-color: white;
-        color: black;
-        cursor: pointer;
+        color: #c7c7c7;
       }
     }
   }
 `;
+const MainSection = styled.div`
+  width: 100vw;
+  height: 100vh;
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <Header>
-          <video autoPlay muted loop id="videoBG">
-            <source src={background} />
-          </video>
-          <p>Bass Guitar Store</p>
-          <p className="exploreButton">
-            <span>Explore</span>
-          </p>
-        </Header>
-      </div>
-    );
+  z-index: 0;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-rows: 1fr 1fr;
+  position: fixed;
+  top: 0;
+  left: 0;
+
+  .item {
+    transform: rotateX(90deg);
+    display: none;
+    background: #e0e0e0;
   }
-}
+`;
+
+const App = () => {
+  const ExploreShow = () => {
+    gsap.to(".item", {
+      duration: 1,
+      rotationX: 0,
+      display: "grid",
+      stagger: 0.1,
+    });
+    gsap.to(".explorebutton", {
+      zIndex: -1,
+    });
+  };
+
+  return (
+    <div className="App">
+      <Header>
+        <video autoPlay muted loop id="videoBG">
+          <source src={background} />
+        </video>
+        <p>Bass Guitar Store</p>
+        <p className="exploreButton">
+          <button className="explorebutton" onClick={ExploreShow}>
+            Explore
+          </button>
+        </p>
+      </Header>
+      <MainSection className="MainSection">
+        <div className="item item1"></div>
+        <div className="item item2"></div>
+        <div className="item item3"></div>
+        <div className="item item4"></div>
+        <div className="item item5"></div>
+        <div className="item item6"></div>
+      </MainSection>
+    </div>
+  );
+};
 
 export default App;
